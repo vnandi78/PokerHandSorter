c380219967d8bd54950538b34fe5f92c81138ed5
VDRKumar <Venkata.Nandi78@gmail.com>
2017-02-23 17:57:24 +1100

    Added comments to royal flush scenario

--- PokerHandSorter/src/com/icm/pokerhandsorter/winner/criterion/RoyalFlushCriterion.java
+++ PokerHandSorter/src/com/icm/pokerhandsorter/winner/criterion/RoyalFlushCriterion.java
@@ -20,11 +20,18 @@ public class RoyalFlushCriterion {
 		LinkedHashSet<String> numberSet = new LinkedHashSet<String>();
 		
 		for(Card card: playerCards){
+			//----Take all the card suites into a set, that will eliminate all duplicates, 
+			//----resulting to finding that all cards have same suite
 			suiteSet.add(card.getSuite());
+			
+			//----Similarly, Take all the card numbers into a set, that will eliminate all duplicates, 
+			//----resulting to finding that all cards have unique number
 			numberSet.add(card.getNumber());
 		}
 		
+		//-----So, if suite set size is 1, then we can proceed to check the number set to be unique
 		if(suiteSet.size()==1 && numberSet.size() == 5){
+			//-- If the number set is unique and has all 5 high cards, then Royal Flush met
 			if(numberSet.contains("A") 
 				&& numberSet.contains("Q") 
 				&& numberSet.contains("K") 
@@ -40,9 +47,11 @@ public class RoyalFlushCriterion {
 
 	public static Player determineRoyalFlushWinner(Player player1, Player player2){
 		
+		//--If royal flush scenario is true for player 1 then return him to winning podium
 		if(isItRoyalFlushScenario(player1.getCards())){
 			return player1;
 		}
+		//--If royal flush scenario is true for player 2 then return him to winning podium
 		else if(isItRoyalFlushScenario(player2.getCards())){
 			return player2;
 		}
