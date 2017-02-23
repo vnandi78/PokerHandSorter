d040014c3c548dd13a57d1e3d2f4a59d3702de2e
VDRKumar <Venkata.Nandi78@gmail.com>
2017-02-23 17:11:07 +1100

    Done with royal flush criterion

--- PokerHandSorter/src/com/icm/pokerhandsorter/mainApp/MyPokerSolution.java
+++ PokerHandSorter/src/com/icm/pokerhandsorter/mainApp/MyPokerSolution.java
@@ -17,31 +17,44 @@ public class MyPokerSolution {
 				
 		Scanner scanner = new Scanner(System.in);
 		
+		//---Build Player 1
 		Player player1 = new Player();
 		player1.setName("Player1");
 		
+		//---Build Player 2
 		Player player2 = new Player();
 		player2.setName("Player2");
 		
+		//---Init Cards for Player 1
 		ArrayList<Card> player1cards = new ArrayList<Card>();
+
+		//---Init Cards for Player 2
 		ArrayList<Card> player2cards = new ArrayList<Card>();
 		
+		//--Reading input from command line
 		while(scanner.hasNextLine()){
+			//--Reading each line
 			String line = scanner.nextLine().trim();
+			//--Split each line by space to collect each card information
 			String[] inputArray = line.split("\\s");
 			System.out.println("inputArray = " + inputArray);
 			
 			int i=0;
 			
 			for(String input: inputArray){
+				//----build card with the given information
 				Card card = new Card();
+				//---Assign card the given number
 				card.setNumber(input.substring(0,1));
+				//---Assign card the given suite
 				card.setSuite(input.substring(1));
 		
 				if(i<5){
+					//--First 5 cards always belong to Player 1
 					player1cards.add(card);
 				}
 				else if(i>4 && i<10){
+					//--Next 5 cards always belong to Player 2
 					player2cards.add(card);					
 				}
 				else{
@@ -52,7 +65,9 @@ public class MyPokerSolution {
 			}
 		}
 
+		//----Assign the complete set of cards for Player 1 from the given input
 		player1.setCards(player1cards);
+		//----Assign the complete set of cards for Player 2 from the given input
 		player2.setCards(player2cards);
 		
 		//------------------------------------Determine the best hand and get their scores-------------------
--- PokerHandSorter/src/com/icm/pokerhandsorter/winner/criterion/RoyalFlushCriterion.java
+++ PokerHandSorter/src/com/icm/pokerhandsorter/winner/criterion/RoyalFlushCriterion.java
@@ -3,6 +3,7 @@
  */
 package com.icm.pokerhandsorter.winner.criterion;
 
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import com.icm.pokerhandsorter.cards.Card;
@@ -15,10 +16,37 @@ import com.icm.pokerhandsorter.players.Player;
 public class RoyalFlushCriterion {
 	
 	public static boolean isItRoyalFlushScenario(List<Card> playerCards){
+		LinkedHashSet<String> suiteSet = new LinkedHashSet<String>();
+		LinkedHashSet<String> numberSet = new LinkedHashSet<String>();
+		
+		for(Card card: playerCards){
+			suiteSet.add(card.getSuite());
+			numberSet.add(card.getNumber());
+		}
+		
+		if(suiteSet.size()==1 && numberSet.size() == 5){
+			if(numberSet.contains("A") 
+				&& numberSet.contains("Q") 
+				&& numberSet.contains("K") 
+				&& numberSet.contains("J") 
+				&& numberSet.contains("10")){
+				System.out.println("hurray!! he is lucky, a gold flush found......");
+				return true;
+			}
+		}
+		
 		return false;
 	}
 
 	public static Player determineRoyalFlushWinner(Player player1, Player player2){
+		
+		if(isItRoyalFlushScenario(player1.getCards())){
+			return player1;
+		}
+		else if(isItRoyalFlushScenario(player2.getCards())){
+			return player2;
+		}
+		
 		return null;
 	}
 
